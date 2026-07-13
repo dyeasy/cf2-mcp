429948464ceb47aff11d018d02cd72ace6c77654
jiangxin <jx3313@163.com>
2026-07-13 22:27:19 +0800

    重构 create_business_dir 函数，使用 WalkDir 替代 read_dir，优化目录遍历逻辑并添加文件内容读取

--- src/tools/createbusinessdir.rs
+++ src/tools/createbusinessdir.rs
@@ -4,12 +4,16 @@
  * @Company: orientsec.com.cn
  * @Description:
  */
-use fs_extra::{copy_items, dir::{CopyOptions, copy, create}};
+use fs_extra::{
+    copy_items,
+    dir::{CopyOptions, copy, create},
+};
 use rmcp::handler::server::wrapper::Parameters;
 use std::{
-    fs::{self, read_dir},
+    fs::{self, read_to_string},
     path::{Path, PathBuf},
 };
+use walkdir::{DirEntry, WalkDir};
 
 #[derive(Debug, serde::Deserialize, schemars::JsonSchema)]
 pub struct CreateFolderBusinessParams {
@@ -39,19 +43,34 @@ pub async fn create_business_dir(
 
     let _create = create(path_ref, bool::default());
 
-    let entries: Vec<PathBuf> = fs::read_dir(code_template_path_url)
-        .ok() // 1. Result -> Option<ReadDir> (如果出错就变 None)
-        .into_iter() // 2. Option<ReadDir> -> Iterator (包含 1 个或 0 个 ReadDir)
-        .flatten() // 3. 将 Iterator<ReadDir> 压平为 Iterator<DirEntry>
-        .filter_map(|f| f.ok()) // 4. 过滤掉读取 DirEntry 过程中的内部错误
-        .map(|f| f.path()) // 5. 提取路径
-        .collect();
+    let dirs = WalkDir::new(code_template_path_url)
+        .min_depth(1)
+        .max_depth(2)
+        .into_iter()
+        .filter_map(|f| f.ok())
+        .map(|p| p.into_path());
 
-    let options = CopyOptions {
-        copy_inside: true, // 如果目标已存在，拷贝到里面
-        overwrite: true,   // 允许覆盖
-        ..Default::default()
-    };
-    let _ccc=copy_items(&entries, path_ref, &options);
+    dirs.for_each(|path| {
+        if path.is_file() {
+            let Ok(code) = read_to_string(path) else {
+                return;
+            };
+            println!("code :{}", code)
+        }
+    });
+    // let entries: Vec<PathBuf> = read_dir(code_template_path_url)
+    //     .ok() // 1. Result -> Option<ReadDir> (如果出错就变 None)
+    //     .into_iter() // 2. Option<ReadDir> -> Iterator (包含 1 个或 0 个 ReadDir)
+    //     .flatten() // 3. 将 Iterator<ReadDir> 压平为 Iterator<DirEntry>
+    //     .filter_map(|f| f.ok()) // 4. 过滤掉读取 DirEntry 过程中的内部错误
+    //     .map(|f| f.path()) // 5. 提取路径
+    //     .collect();
+    // println!("aaaa:{:?}",entries);
+    // let options = CopyOptions {
+    //     copy_inside: true, // 如果目标已存在，拷贝到里面
+    //     overwrite: true,   // 允许覆盖
+    //     ..Default::default()
+    // };
+    // let _ccc=copy_items(&entries, path_ref, &options);
     "afdsafd".to_string()
 }
